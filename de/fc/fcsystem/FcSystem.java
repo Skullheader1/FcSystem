8a908f162b945ddd2933468c0874a49f9434b962
HighDxde <Davudsa8@gmail.com>
2023-03-09 19:19:07 +0100

    Initial commit

---
+++ de/fc/fcsystem/FcSystem.java
@@ -0,0 +1,17 @@
+package de.fc.fcsystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FcSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
